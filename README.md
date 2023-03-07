03a4c4fd383b3a7e8e7d388d483443a21d0d14a8
wtrocki <w.trocki@mongodb.com>
2023-03-07 19:05:21 +0100

    fix: base tools

---
+++ README.md
@@ -0,0 +1,16 @@
+#  Atlas Client for Python
+
+> NOTE: This project is just an experiment. It should not be used by general public.
+
+Generated pyton client for atlas.
+
+## Contents
+
+Project contains:
+
+- Github actions automation for OpenAPI processing and generation
+- OpenAPI processing tool for transforming OpenAPI schema
+- OpenAPI generator setup with various scripts helping with generation
+
+See [tools](./tools) for more info.
+
---
+++ tools/transformer/src/transformations/index.js
@@ -0,0 +1,16 @@
+const { applyAllOfTransformations, transformAllOf } = require("./allOf");
+const { applyModelNameTransformations } = require("./name");
+const { applyOneOfTransformations, transformOneOf } = require("./oneOf");
+const { applyDiscriminatorTransformations } = require("./discriminator");
+
+const { applyDigestTransformations } = require("./digest");
+
+module.exports = {
+  applyModelNameTransformations,
+  transformAllOf,
+  transformOneOf,
+  applyAllOfTransformations,
+  applyOneOfTransformations,
+  applyDigestTransformations,
+  applyDiscriminatorTransformations,
+};
